5c8254e8c1c1522b6118b31ebca0435eb6a6d270
Apple12345123 <pgh3621@naver.som>
2024-02-28 09:51:51 +0900

    Day 14

---
+++ src/polymorphism/interface0/car/poly/Driver.java
@@ -0,0 +1,36 @@
+package polymorphism.interface0.car.poly;
+
+// 운전자는 Avante를 소유하고, 운전을 할 수 있다.
+public class Driver {
+    // 필드
+    private Avante avante;
+    // 차량이 추가되면 코드가 변경되어야 한다.
+    private Sonata sonata;
+
+    // 메서드
+
+    // 자동차를 설정
+    public void setAvante(Avante avante) {      // set 치고 Enter
+        this.avante = avante;
+    }
+
+    public void setSonata(Sonata sonata) {
+        this.sonata = sonata;
+    }
+
+    // 운전
+    public void drive() {
+        if (avante !=null) {
+            avante.onEngine();
+            avante.pressAccel();
+            avante.pressBread();
+            avante.offEngine();
+        } else if (sonata != null) {
+            sonata.onEngine();
+            sonata.pressAccel();
+            sonata.pressBread();
+            sonata.offEngine();
+        }
+
+    }
+}
