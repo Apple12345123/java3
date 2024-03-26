7ed7aa9b5ce6244f99cf6af90fd543ad94234c74
Apple12345123 <pgh3621@naver.som>
2024-03-26 15:50:50 +0900

    Day 32

---
+++ src/io_stream/IOStream2.java
@@ -0,0 +1,29 @@
+package io_stream;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class IOStream2 {
+    public static void main(String[] args) {
+        try {
+            OutputStream outputStream = new FileOutputStream("src/io_stream/test2.txt");
+
+            byte[] byteArray = {65, 66, 67};
+
+            // 배열의 모든 바이트를 출력
+            outputStream.write(byteArray);
+
+            // 버퍼 지우기
+            outputStream.flush();
+
+            // 메모리 해제
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
